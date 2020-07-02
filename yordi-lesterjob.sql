11184f3b04dedeeb4684d8890c7e98a8365b07fa
Snap <64895070+SnapDragon-cmd@users.noreply.github.com>
2020-07-02 13:13:35 +0300

    Update yordi-lesterjob.sql

--- yordi-lesterjob.sql
+++ yordi-lesterjob.sql
@@ -1,5 +1,4 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-	('yPistol', 'Pistol', 3, 0, 1)
-	('yMicroSMG', 'Micro SMG', 3, 0, 1)
-	('ySMG', 'SMG', 3, 0, 1)
-;
+	('yPistol', 'Pistol', 3, 0, 1),
+	('yMicroSMG', 'Micro SMG', 3, 0, 1),
+	('ySMG', 'SMG', 3, 0, 1);
